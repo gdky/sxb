0eb5fc5fe832896616ea08e58d00089205ccd442
gdkyJoy <kai.zhang@gdkyit.com>
2016-07-12 10:08:03 +0800

    2016 7 12
    zskly gxtjgl yxtk zszsk

---
+++ src/main/java/gov/hygs/htgl/utils/AttachmentOpt.java
@@ -0,0 +1,21 @@
+package gov.hygs.htgl.utils;
+
+import java.io.File;
+
+public class AttachmentOpt {
+	public static String getAttachmentPath(){
+		//String path = "/user/local/tomcat/app/";
+		return "E:\\user\\local\\tomcat\\app\\";
+	}
+	
+	public static boolean deleteAttachmentFile(String param){
+		String path = AttachmentOpt.getAttachmentPath();
+		File file  = new File(path+param);
+		if(file.exists()){
+			if(file.isFile()){
+				return file.delete();
+			}
+		}
+		return false;
+	}
+}
