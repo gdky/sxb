b8ad6f812dc99ebde4deb4edebcd3cd6e14ea04c
gdkyJoy <kai.zhang@gdkyit.com>
2016-07-13 08:54:52 +0800

    2016 7 13
    kstsjl tj

--- src/main/java/gov/hygs/htgl/service/impl/TjglServiceImpl.java
+++ src/main/java/gov/hygs/htgl/service/impl/TjglServiceImpl.java
@@ -17,13 +17,29 @@ public class TjglServiceImpl implements TjglService {
 	@Override
 	public List countGxjl(Map<String, Object> param) {
 		// TODO Auto-generated method stub
-		Object type = param.get("type");
-		if("0".equals(type) || type == null){
+		//Object type = param.get("type");
+		/*if("0".equals(type) || type == null){
 			return tjglDao.countGxjl(param);
 		}else if("1".equals(type)){
 			return tjglDao.countZskgxjl(param);
 		}else if("2".equals(type)){
 			return tjglDao.countLaudRecord(param);
+		}*/
+		Object type = (param==null)?param:param.get("type");
+		if("0".equals(type) || type == null){//部门题库贡献记录
+			return tjglDao.countDeptGxjl(param);
+		}else if("1".equals(type)){//个人题库贡献记录
+			return tjglDao.countGxjl(param);
+		}else if("2".equals(type)){//部门知识库贡献记录
+			return tjglDao.countDeptZskgxjl(param);
+		}else if("3".equals(type)){//个人知识库贡献记录
+			return tjglDao.countZskgxjl(param);
+		}else if("4".equals(type)){//部门点赞数
+			return tjglDao.countDeptLaudRecord(param);
+		}else if("5".equals(type)){//个人点赞数
+			return tjglDao.countLaudRecord(param);
+		}else if("6".equals(type)){//题目点赞数
+			return tjglDao.countTktmLaudRecord(param);
 		}
 		return null;
 	}
